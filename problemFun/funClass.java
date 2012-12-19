71ce35bea3f7adb34a30f19c5d40aa2e7090cca9
Devin Hurley <snowmiser111@mac.com>
2012-12-18 19:28:47 -0500

    test folder

---
+++ problemFun/funClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class funClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class funClass
+{
+    public static void main(String args[]){
+        System.out.println("FUN!!!");
+    }
+}
